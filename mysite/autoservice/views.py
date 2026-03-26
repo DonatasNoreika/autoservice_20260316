c421ec9bc3f0083b65c5f60834b5e4ed0fdc4c07
DonatasNoreika <donoras@gmail.com>
2026-03-26 08:05:13 +0200

    7: +Padaryti, kad pagrindiniame puslapyje būtų rodomas sesijos apsilankymų skaičius.

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -5,10 +5,13 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 
 def index(request):
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'services': Service.objects.count(),
         'cars': Car.objects.count(),
         'orders_done': Order.objects.filter(status='c').count(),
+        'num_visits': num_visits,
     }
     return render(request, template_name="index.html", context=context)
 
