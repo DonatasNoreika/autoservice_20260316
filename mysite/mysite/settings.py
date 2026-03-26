d1c725a8ba592a146360597798f50cece16321aa
DonatasNoreika <donoras@gmail.com>
2026-03-26 08:26:33 +0200

    7: +Įgyvendintas emailų saugojimas į failus

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -132,7 +132,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
